8754f41350f5f66ca75e1b97ff84cb879936795b
shoxxdj <remi.martin-de-abia@advens.fr>
2022-06-07 10:40:05 +0200

    update

--- tibis/lib/config.py
+++
@@ -1,7 +0,0 @@
-import yaml 
-import tibis.lib.static as static
-
-def storage_path():
-    with open(static.tibis_full_config_location) as file:
-        p = yaml.load(file,Loader=yaml.FullLoader)
-        return p['storage_path']
