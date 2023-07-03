475e8fc51507d41f53b16a24f115e11b929c86e3
shoxxdj <remi.martin-de-abia@advens.fr>
2023-07-03 14:44:09 +0200

    fix bug with check_integrity

--- tibis/lib/config.py
+++ tibis/lib/config.py
@@ -1,5 +1,6 @@
 import yaml 
 import tibis.lib.static as static
+import sys
 
 def storage_path():
     with open(static.tibis_full_config_location) as file:
@@ -10,3 +11,11 @@ def pgp_infos():
     with open(static.tibis_full_config_location) as file:
         p = yaml.load(file,Loader=yaml.FullLoader)
         return p['pgp_infos']
+
+def check_integrity_status():
+    with open(static.tibis_full_config_location) as file:
+        p = yaml.load(file,Loader=yaml.FullLoader)
+        if('check_integrity' in p ):
+            return p['check_integrity']
+        else:
+            return True
